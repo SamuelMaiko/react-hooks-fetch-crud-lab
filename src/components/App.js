1acc1a2665e92daac47bc2f5b582928fcb73ad52
Samuel Maiko <sammymaiko721@gmail.com>
2023-07-24 12:37:31 +0300

    lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,25 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import AdminNavBar from "./AdminNavBar";
 import QuestionForm from "./QuestionForm";
 import QuestionList from "./QuestionList";
 
 function App() {
   const [page, setPage] = useState("List");
+  const [questions, setquestions] = useState([])
 
+  useEffect(()=>{
+    fetchData()
+  },[])
+
+  const fetchData=()=>{
+    fetch('http://localhost:4000/questions')
+    .then(response=>response.json())
+    .then(data=>setquestions(data))
+  }
   return (
     <main>
       <AdminNavBar onChangePage={setPage} />
-      {page === "Form" ? <QuestionForm /> : <QuestionList />}
+      {page === "Form" ? <QuestionForm /> : <QuestionList onquestions={questions} onsetquestions={setquestions}/>}
     </main>
   );
 }
